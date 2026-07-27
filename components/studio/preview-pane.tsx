59c285554383251a38cb0ddcce06896c596fa928
kapishdima <kapishdima@gmail.com>
2026-07-27 23:14:43 +0300

    fix: linter

--- components/studio/preview-pane.tsx
+++ components/studio/preview-pane.tsx
@@ -6,7 +6,6 @@ import {
   MonitorPlayIcon,
   RotateCwIcon,
 } from "lucide-react";
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Empty,
